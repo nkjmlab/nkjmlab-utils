17d68b95413b081b697824215d682dbf5a6bcb6f
Yuu Nakajima <yuu.nkjm.pub@gmail.com>
2021-12-08 16:27:10 +0900

    Update comment

--- nkjmlab-utils-core/src/main/java/org/nkjmlab/util/java/json/FileDatabaseConfigJson.java
+++ nkjmlab-utils-core/src/main/java/org/nkjmlab/util/java/json/FileDatabaseConfigJson.java
@@ -3,7 +3,7 @@ package org.nkjmlab.util.java.json;
 import java.io.File;
 
 /**
- * The class is a container for file database setting JSON.
+ * A container for file database setting JSON.
  *
  * @author nkjm
  *
