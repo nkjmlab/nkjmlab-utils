febdf9ac1f15a7e1568ae277c8a06a7fbb5d4b62
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2022-04-04 10:48:31 +0900

    Modify ArrayUtils

--- nkjmlab-utils-core/src/main/java/org/nkjmlab/util/h2/H2SqlUtils.java
+++ nkjmlab-utils-core/src/main/java/org/nkjmlab/util/h2/H2SqlUtils.java
@@ -1,20 +1,17 @@
-package org.nkjmlab.sorm4j.util.h2;
+package org.nkjmlab.util.h2;
 
 import static java.lang.String.*;
 
 public class H2SqlUtils {
 
-
   public static String getRenameTableSql(String fromTableName, String toTableName) {
     return "ALTER TABLE " + fromTableName + " RENAME TO " + toTableName;
   }
-
 
   public static String getCreateIndexOnSql(String tableName, String... columns) {
     return "create index if not exists " + "index_" + tableName + "_" + join("_", columns) + " on "
         + tableName + "(" + String.join(", ", columns) + ")";
   }
-
 
   public static String dropTablesCascadeIfExist(String tableName) {
     return "DROP TABLE IF EXISTS " + tableName + " CASCADE";
--- nkjmlab-utils-helper/src/test/java/org/nkjmlab/util/h2/H2StatementUtilsTest.java
+++ nkjmlab-utils-helper/src/test/java/org/nkjmlab/util/h2/H2StatementUtilsTest.java
@@ -11,14 +11,14 @@ class H2StatementUtilsTest {
   @Test
   void testGetCsvWriteSql() {
     String ret = H2CsvFunctions.getCallCsvWriteSql(new File("hoge.csv"), "select * from test_table",
-        StandardCharsets.UTF_8, ",");
+        StandardCharsets.UTF_8, ',');
     System.out.println(ret);
   }
 
   @Test
   void testGetCsvReadSql() {
     String ret =
-        H2CsvFunctions.getCsvReadSql(new File("hoge.csv"), null, StandardCharsets.UTF_8, ",");
+        H2CsvFunctions.getCsvReadSql(new File("hoge.csv"), null, StandardCharsets.UTF_8, ',');
     System.out.println(ret);
   }
 
