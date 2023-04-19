f11421c6d0b2b50b139801ff1e1a62d95a3d3bbe
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2023-04-19 15:01:26 +0900

    Update LogManager and add JulLogger.java

--- nkjmlab-utils-core/src/main/java/org/nkjmlab/util/java/logging/LogManager.java
+++ nkjmlab-utils-core/src/main/java/org/nkjmlab/util/java/logging/LogManager.java
@@ -1,44 +1,35 @@
 package org.nkjmlab.util.java.logging;
 
 import org.nkjmlab.util.java.function.Try;
-import org.nkjmlab.util.log4j.Log4jLogger;
 
 public class LogManager {
-  static {
-    isEnableLog4jApi();
-  }
   private static final boolean enableLog4jCore = isEnableLog4jCore();
 
-  private static boolean isEnableLog4jApi() {
+  private static boolean isEnableLog4jCore() {
+    String ORG_APACHE_LOGGING_LOG4J_CORE_LOGGER = "org.apache.logging.log4j.core.Logger";
     boolean ret = Try.getOrElse(() -> {
-      Class.forName("org.apache.logging.log4j.Logger");
+      Class.forName(ORG_APACHE_LOGGING_LOG4J_CORE_LOGGER);
       return true;
     }, false);
     if (!ret) {
-      System.err.println("WARNING " + LogManager.class.getName()
-          + " could not find a logging implementation of log4j2. Please add log4j-core to the classpath. "
-          + "No operation logger (" + NopLogger.class.getSimpleName() + ")  will be used...");
+      System.err.println("nkjmlab-utils-core: WARNING " + "Failed to load class '"
+          + ORG_APACHE_LOGGING_LOG4J_CORE_LOGGER
+          + "'. If you need the log of nkjmlab-utils-core library, add log4j-core to the classpath.");
+      System.err.println("nkjmlab-utils-core: WARNING " + "No operation logger will be used.");
     }
     return ret;
   }
 
-  private static boolean isEnableLog4jCore() {
-    return Try.getOrElse(() -> {
-      Class.forName("org.apache.logging.log4j.core.Logger");
-      return true;
-    }, false);
-  }
-
   public static SimpleLogger createLogger() {
-    return enableLog4jCore ? new Log4jLogger() : NopLogger.INSTANCE;
+    return enableLog4jCore ? new org.nkjmlab.util.log4j.Log4jLogger() : NopLogger.INSTANCE;
   }
 
   public static SimpleLogger createLogger(org.apache.logging.log4j.Logger log) {
-    return new Log4jLogger(log);
+    return enableLog4jCore ? new org.nkjmlab.util.log4j.Log4jLogger(log) : NopLogger.INSTANCE;
   }
 
   public static SimpleLogger createLogger(String name) {
-    return new Log4jLogger(name);
+    return enableLog4jCore ? new org.nkjmlab.util.log4j.Log4jLogger(name) : NopLogger.INSTANCE;
   }
 
 
