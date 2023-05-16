454abad9b40c2a8de978255c0fc91909f395e700
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2023-05-16 17:56:02 +0900

    Update ResourceUtils

--- nkjmlab-utils-core/src/main/java/org/nkjmlab/util/java/lang/ResourceUtils.java
+++ nkjmlab-utils-core/src/main/java/org/nkjmlab/util/java/lang/ResourceUtils.java
@@ -1,17 +1,80 @@
 package org.nkjmlab.util.java.lang;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import org.nkjmlab.util.java.function.Try;
+import org.nkjmlab.util.java.io.SystemFileUtils;
 
 public class ResourceUtils {
+
+  public static List<String> readAllLines(Class<?> clazz, String resourcePath) {
+    return readAllLinesFromResource(clazz, resourcePath, StandardCharsets.UTF_8);
+  }
+
+  public static List<String> readAllLinesFromResource(Class<?> clazz, String resourcePath,
+      Charset cs) {
+    try (InputStream in = clazz.getResourceAsStream(resourcePath);
+        InputStreamReader ir = new InputStreamReader(in, cs);
+        BufferedReader br = new BufferedReader(ir)) {
+      List<String> ret = new ArrayList<>();
+      String line;
+      while ((line = br.readLine()) != null) {
+        ret.add(line);
+      }
+      return ret;
+    } catch (IOException e) {
+      throw Try.rethrow(e);
+    }
+  }
+
+
+  public static File copyResourceToTempDir(Class<?> clazz, String resourcePath) {
+    byte[] bytes = readAllByteFromResource(clazz, resourcePath);
+    String[] pathElements = resourcePath.split("/");
+
+    File parent = new File(SystemFileUtils.getTempDirectory(), clazz.getCanonicalName());
+    File destFile =
+        new File(parent, System.currentTimeMillis() + "-" + pathElements[pathElements.length - 1]);
+    parent.mkdirs();
+
+    try (FileOutputStream out = new FileOutputStream(destFile)) {
+      out.write(bytes);
+    } catch (IOException e) {
+      throw Try.rethrow(e);
+    }
+    return destFile;
+  }
+
+  public static byte[] readAllByteFromResource(Class<?> clazz, String resourcePath) {
+    try (InputStream in = clazz.getResourceAsStream(resourcePath);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+
+      byte[] buffer = new byte[1024];
+      int bytesRead;
+
+      while ((bytesRead = in.read(buffer)) != -1) {
+        outputStream.write(buffer, 0, bytesRead);
+      }
+
+      return outputStream.toByteArray();
+
+    } catch (IOException e) {
+      throw Try.rethrow(e);
+    }
+  }
+
+
 
   public static File getResourceAsFile(Class<?> clazz, String resourceName) {
     return new File(getResourceAsUri(clazz, resourceName));
@@ -20,8 +83,6 @@ public class ResourceUtils {
   public static File getResourceAsFile(String resourceName) {
     return getResourceAsFile(ResourceUtils.class, resourceName);
   }
-
-
 
   public static URI getResourceAsUri(Class<?> clazz, String resourceName) {
     try {
@@ -33,22 +94,6 @@ public class ResourceUtils {
 
   public static URI getResourceAsUri(String file) {
     return getResourceAsUri(ResourceUtils.class, file);
-  }
-
-  public static List<String> readAllLines(Class<?> clazz, String resourceName) {
-    List<String> lines = new ArrayList<>();
-    try (InputStream in = clazz.getResourceAsStream(resourceName);
-        InputStreamReader ir = new InputStreamReader(in);
-        BufferedReader br = new BufferedReader(ir)) {
-      String line;
-      while ((line = br.readLine()) != null) {
-        lines.add(line);
-      }
-    } catch (IOException e) {
-      throw Try.rethrow(e);
-    }
-    return lines;
-
   }
 
   public static String toResourceName(File file) {
